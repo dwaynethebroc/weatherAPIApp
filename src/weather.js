5a8a8e0bb298e02b0b10d44787c03fd4c4cc3b02
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-12-15 15:58:39 +1100

    fix API query

--- src/weather.js
+++ src/weather.js
@@ -4,22 +4,20 @@ export { getWeatherDataCityCountry, getWeatherDataGeoLocation}
 
 const getWeatherDataCityCountry = async function(){
     const location = readLocationSearch();
-    const city = location.city;
-    const country = location.country;
 
     const key = "P7RMSP9B8LK52KS5VP79GS5TB";
     const APIrequestURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
-    console.log(`${APIrequestURL + city},${country}?key=${key}`)
+    console.log(`${APIrequestURL + location.city},${location.country}?key=${key}`)
 
-    const response = await fetch(`${APIrequestURL + city},${country}?key=${key}`, {mode: "cors"})
+    const response = await fetch(`${APIrequestURL + location.city},${location.country}?key=${key}`, {mode: "cors"})
         .catch(onFailure);
     const geoData = await response.json();
     
     return geoData;
 }
 
-const shareLocation = async function(){
+const shareLocation = function(){
     const latLongData = {};
     
     navigator.geolocation.getCurrentPosition((position) => {
@@ -34,18 +32,16 @@ const shareLocation = async function(){
 
 const getWeatherDataGeoLocation = async function(){
     const locationData = await shareLocation(); 
-
-    console.log(locationData.latitude);
-    console.log(locationData.longitude);
+    console.log(locationData);
 
     const key = "P7RMSP9B8LK52KS5VP79GS5TB";
     const APIrequestURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+    
     console.log(`${APIrequestURL + locationData.latitude},${locationData.longitude}?unitGroup=metric&key=${key}`);
 
     const response = await fetch(`${APIrequestURL + locationData.latitude},${locationData.longitude}?key=${key}`, {mode: "cors"});
     const weatherData = await response.json();
 
-    console.log(weatherData);
 }
 
 const readLocationSearch = function(){
