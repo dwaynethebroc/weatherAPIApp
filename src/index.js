5c6094667d3cd2a5edb76c76deaf7fb17ecff993
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2024-12-23 14:41:44 +1100

    Deployment commmit

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./styles.css";
-import { verifyString, getWeatherDataGeoLocation} from "./weather.js";
-
-const searchButton = document.getElementById('searchButton');
-searchButton.addEventListener('click', verifyString);
-
-const shareLocationButton = document.getElementById('shareLocation');
-shareLocationButton.addEventListener('click', getWeatherDataGeoLocation);
